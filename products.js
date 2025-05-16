1e31dcda64c42d05ff067c795991144c0e977923
jovicastro <79239650+jovicastro@users.noreply.github.com>
2025-05-16 01:19:39 -0300

    Product Card

--- products.js
+++ products.js
@@ -1,7 +1,15 @@
     const produtos = [
+<<<<<<< HEAD
       { name: "Camiseta Preta", price: 49.99, imageUrl: "bg.jpg" },
       { name: "Tênis Esportivo", price: 999.90, imageUrl: "bg.jpg" },
       { name: "Mochila Escolar", price: 89.50, imageUrl: "bg.jpg" }
+=======
+      { name: "Camiseta Preta", price: 49.99, imageUrl: "bg.jpg", local: "Guarulhos", data: "25/12/2024, 00:00" },
+      { name: "Tênis Esportivo", price: 999.90, imageUrl: "bg.jpg", local: "Guarulhos", data: "25/12/2024, 00:00" },
+      { name: "Mochila Escolar", price: 89.50, imageUrl: "png.webp", local: "Guarulhos", data: "25/12/2024, 00:00"},
+      { name: "Mochila Escolar", price: 89.50, imageUrl: "8136031.png", local: "Guarulhos", data: "25/12/2024, 00:00"},
+      { name: "Mochila Escolar", price: 89.50, imageUrl: "png.jpg", local: "Guarulhos", data: "25/12/2024, 00:00"}
+>>>>>>> 58c04ee (product cards)
     ];
 
     const container = document.getElementById("products");
@@ -14,6 +22,12 @@
 
       clone.querySelector(".productName").textContent = produto.name;
       clone.querySelector(".productPrice").textContent = `R$ ${produto.price.toFixed(2)}`;
+<<<<<<< HEAD
+=======
+      clone.querySelector(".local").textContent = produto.local;
+      clone.querySelector(".data").textContent = produto.data;
+      
+>>>>>>> 58c04ee (product cards)
       clone.querySelector("img").src = produto.imageUrl;
 
       container.appendChild(clone);
